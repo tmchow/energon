da484e9f5d1ca76f7706f1f2376d0cff3e54ef94
Trevin Chow <trevin@trevinchow.com>
2026-09-01 17:08:35 -0700

    Port site/file claim checks and purge race tests from Esper-Labs/energon#15.

--- src/sites.ts
+++ src/sites.ts
@@ -1,1 +1,39 @@
-PLACEHOLDER_READ_FROM_DISK
+import { privateCacheControl, publicCacheControl, purgeContent, siteCacheTag, sitePrefix } from "./cache";
+import {
+  involvementSql,
+  likeNeedle,
+  nextSiteCursor,
+  siteCursorSql,
+  takePage,
+  type ListPage,
+  type ListQuery,
+} from "./catalog";
+import { brandMark, documentShell } from "./chrome";
+import { MAX_IMPORT_FILES, PRODUCT, RESERVED_SLUGS, SLUG_RE, formatBytes, siteKey } from "./config";
+import {
+  expiredError,
+  expiredHtml,
+  isExpired,
+  isPurgeClaimed,
+  PURGE_CLAIM_LIKE,
+  purgeExpiredSite,
+  remainingCacheSeconds,
+  schedulePurgeExpiredSite,
+} from "./expire";
+import { isMarkdownName, respondMarkdown } from "./markdown";
+import { passwordEcho, passwordHashFromInput, protectContent } from "./gate";
+import { ensureHandle, ensureUser } from "./handles";
+import { ApiError, assertStorageRoom, basename, contentDisposition, copyR2Object, deletePrefix, htmlPage, json, normalizeRelPath, publicOrigin, tooLarge, wantsDownload } from "./http";
+import { contentTypeFor } from "./mime";
+import {
+  assertCanMutate,
+  assertCanSetWritePolicy,
+  instancePolicy,
+  requestedWritePolicy,
+  resolveCreateWritePolicy,
+  resolveExpiresAt,
+  resolveWritePolicy,
+} from "./policy";
+import type { Actor, Env, SiteFileRow, SiteRow } from "./types";
+import { sitePublicUrl } from "./urls";
+import { packZip, unpackZip } from "./zip";
